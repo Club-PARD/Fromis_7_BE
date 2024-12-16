4b711f98ff97637c6f93c394f5df8e11d043059b
hajin <haja5j5j@naver.com>
2024-12-16 18:47:51 +0900

    [Feat]초기 파일 생성

---
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/Fromis7BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fromis_7_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fromis7BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fromis7BeApplication.class, args);
+    }
+
+}
---
+++ fromis_7_be/src/test/java/com/example/fromis_7_be/Fromis7BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fromis_7_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fromis7BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
