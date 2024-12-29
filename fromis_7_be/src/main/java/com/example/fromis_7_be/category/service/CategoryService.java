c7d39d3bf11f8f53196f78f71057c2219de467ef
SuinLee10 <143365454+SuinLee10@users.noreply.github.com>
2024-12-30 05:08:22 +0900

    [수인]: Update Listup (error resolved)

--- fromis_7_be/src/main/java/com/example/fromis_7_be/category/service/CategoryService.java
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/category/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.example.fromis_7_be.category.dto.CategoryResponse;
 import com.example.fromis_7_be.category.entity.Category;
 import com.example.fromis_7_be.category.repository.CategoryRepository;
 import com.example.fromis_7_be.listup.entity.Listup;
+import com.example.fromis_7_be.listup.repository.ListupRepository;
 import com.example.fromis_7_be.listup.service.ListupService;
 import com.example.fromis_7_be.piece.entity.Piece;
 import com.example.fromis_7_be.piece.repository.PieceRepository;
@@ -28,8 +29,8 @@ public class CategoryService {
         Piece piece = pieceRepository.findById(pieceId)
                 .orElseThrow(() -> new NoSuchElementException("찾으시는 piece 정보: " + pieceId + "가 존재하지 않습니다."));
         Category category = Category.from(req.getName(), req.getColor(), false, piece);
-
         categoryRepository.save(category);
+        listupService.deleteListupByCateId(category.getId());
         listupService.createListupByCateId(category.getId(), req.getListups());
     }
 
@@ -48,20 +49,7 @@ public class CategoryService {
                     );
                 }).collect(Collectors.toList());
     }
-    public CategoryResponse.CategoryReadResponse update(Long categoryId, CategoryRequest.CategoryCreateRequest req){
-        Optional<Category> b = categoryRepository.findById(categoryId);
-        Category category = b.get();
-        category.update(req.getName(), req.getColor(), req.getIsHighlighted());
-        categoryRepository.save(category);
 
-        return new CategoryResponse.CategoryReadResponse(
-                category.getId(),
-                category.getName(), // 예: 이름
-                category.getColor(), // 예: 색상
-                category.getIsHighlighted(), // 예: 강조 여부
-                category.getLists()
-        );
-    }
     public void delete(Long categoryId){
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new NoSuchElementException("찾으시는 category 정보: " + categoryId + "가 존재하지 않습니다."));
