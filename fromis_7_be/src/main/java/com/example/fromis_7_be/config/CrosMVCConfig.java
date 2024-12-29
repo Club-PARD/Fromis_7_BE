61fd72bf5d6a2648131a06a381048e002bd49179
hajin <haja5j5j@naver.com>
2024-12-30 04:36:43 +0900

    [하진] modify : crosconfig

--- fromis_7_be/src/main/java/com/example/fromis_7_be/config/CrosMVCConfig.java
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/config/CrosMVCConfig.java
@@ -12,7 +12,6 @@ public class CrosMVCConfig implements WebMvcConfigurer {
 
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedMethods("*")
                 .allowedOrigins("http://localhost:3000");
     }
 }
