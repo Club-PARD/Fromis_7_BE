75dd74f881e18f3e01763f3f2f4442bbbcf3a1be
hajin <haja5j5j@naver.com>
2024-12-30 05:20:50 +0900

    [하진] modify : crosconfig

---
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/config/CrosMVCConfig.java
@@ -0,0 +1,31 @@
+package com.example.fromis_7_be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrosMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
+
+    }
+
+}
+
+/*
+    implementation 'org.springframework.boot:spring-boot-starter-oauth2-client'
+    implementation 'org.springframework.boot:spring-boot-starter-security'
+
+    // JWT
+    implementation 'io.jsonwebtoken:jjwt-api:0.12.3'
+    implementation 'io.jsonwebtoken:jjwt-impl:0.12.3'
+    implementation 'io.jsonwebtoken:jjwt-jackson:0.12.3'
+
+    implementation 'org.springframework.boot:spring-boot-starter-mustache'
+*/
