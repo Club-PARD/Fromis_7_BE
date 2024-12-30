9a37eb74b5904c8002302e2b6e7da9b5a2730ed9
hajin <haja5j5j@naver.com>
2024-12-31 02:19:43 +0900

    [하진] modify : config file

--- fromis_7_be/src/main/java/com/example/fromis_7_be/category/service/CategoryService.java
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/category/service/CategoryService.java
@@ -25,7 +25,7 @@ public class CategoryService {
     private final PieceRepository pieceRepository;
     private final ListupService listupService;
 
-    public CategoryResponse.CategoryReadResponse createCategoryByPieceId(Long pieceId, CategoryRequest.CategoryCreateRequest req){
+    public void createCategoryByPieceId(Long pieceId, CategoryRequest.CategoryCreateRequest req){
         Piece piece = pieceRepository.findById(pieceId)
                 .orElseThrow(() -> new NoSuchElementException("찾으시는 piece 정보: " + pieceId + "가 존재하지 않습니다."));
         Category category = Category.from(req.getName(), req.getColor(), false, piece);
@@ -33,7 +33,6 @@ public class CategoryService {
         listupService.deleteListupByCateId(category.getId());
         listupService.createListupByCateId(category.getId(), req.getListups());
 
-        return
     }
 
     public List<CategoryResponse.CategoryReadResponse> readCategoryByPiece(Long pieceId){
