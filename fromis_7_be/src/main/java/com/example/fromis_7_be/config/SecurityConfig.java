6c61b1b772401ae68227a9cfdbbc07d8612e3a18
hajin <haja5j5j@naver.com>
2024-12-31 03:27:55 +0900

    [하진] modify : create mycontroller & security config

--- fromis_7_be/src/main/java/com/example/fromis_7_be/config/SecurityConfig.java
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     private final PrincipalOauth2UserService principalOauth2UserService;
     private final CorsConfig corsConfig;
