4f83852fe866353fccf5f218001be21da4e4c4b3
SuinLee10 <143365454+SuinLee10@users.noreply.github.com>
2024-12-31 00:08:00 +0900

    [수인] : update CorsMVCConfig

--- fromis_7_be/src/main/java/com/example/fromis_7_be/config/CorsMVCConfig.java
+++ fromis_7_be/src/main/java/com/example/fromis_7_be/config/CorsMVCConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CrosMVCConfig implements WebMvcConfigurer {
+public class CorsMVCConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
